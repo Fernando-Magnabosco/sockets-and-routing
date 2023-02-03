e30215ac8413211fc3d04ce1aaff6f10ae828bb4
Fernando-Magnabosco <fernandomagnabosco1@gmail.com>
2023-02-03 08:47:52 -0300

    ;

--- src/neighborhood.c
+++ src/neighborhood.c
@@ -6,23 +6,24 @@ void *check_neighbors(void *arg)
     while (1)
     {
         usleep(CHECK_NEIGHBORS_DELAY);
+        pthread_mutex_lock(&r.other_routers_lock);
         pthread_mutex_lock(&r.neighbor_list_lock);
     keep_going:
         for (int_list *iterator = r.neighbor_list; iterator; iterator = iterator->next)
         {
-            pthread_mutex_lock(&r.other_routers_lock);
             if (difftime(time(NULL), r.other_routers[iterator->value].last_update) > TIME_OUT)
             {
                 sprintf(log, "Neighbor %d disconnected\n", iterator->value);
                 write_to_log(log);
-                pthread_mutex_unlock(&r.other_routers_lock);
                 pthread_mutex_unlock(&r.neighbor_list_lock);
+                pthread_mutex_unlock(&r.other_routers_lock);
                 disconnect(iterator->value);
+                pthread_mutex_lock(&r.other_routers_lock);
                 pthread_mutex_lock(&r.neighbor_list_lock);
                 goto keep_going;
             }
-            pthread_mutex_unlock(&r.other_routers_lock);
-        }
+                }
         pthread_mutex_unlock(&r.neighbor_list_lock);
+        pthread_mutex_unlock(&r.other_routers_lock);
     }
 }
